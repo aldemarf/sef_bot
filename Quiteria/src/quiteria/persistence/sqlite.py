1da525a39ce21ea27b02d9742891895037de68c0
aldemarf <aldemar.poli@gmail.com>
2019-07-28 00:22:20 -0300

    Implemented Session Table + refactored DB_pATH

--- Quiteria/src/quiteria/persistence/sqlite.py
+++ Quiteria/src/quiteria/persistence/sqlite.py
@@ -1,5 +1,5 @@
 from sqlalchemy import (MetaData, create_engine, Table,
-                        Column, String, Integer, Boolean)
+                        Column, String, Integer, Boolean, Date)
 
 from quiteria.domain.user import User
 
@@ -11,12 +11,21 @@ class SQLiteDB:
     USER_SESSION_TABLE = 'users_sessions'
     ROOM_SCH_TABLE = 'room_schd'
     MAINTENANCE_SCH_TABLE = 'maintenance_schd'
+    SESSIONS_TABLE = 'sessions'
+
+    # SESSION COLUMNS
+    C_SESSION_ID = 'id'
+    C_SESSION_USER_ID = 'user_id'
+    C_SESSION_LAST_ACCESS = 'last_acess'
+    C_SESSION_ISLOGGED = 'is_logged'
+
 
     # USER COLUMNS
     C_USER_ID = 'id'
-    C_USER_NAME = 'name'
     C_USER_TG_ID = 'telegram_id'
+    C_USER_NAME = 'name'
     C_USER_EMAIL = 'email'
+    C_USER_PWD = 'pwd'
     C_USER_LAB = 'lab'
     C_USER_ROLE = 'role'
     C_USER_ACTIVE = 'isActive'
@@ -27,8 +36,31 @@ class SQLiteDB:
     # MAINTENANCE COLUMNS
     C_MNTNNC_ID = 'id'
 
+    # ## DATABASE PATHS ###
     DB_MEMORY = 'sqlite:///:memory:'
     DB_FILE = 'sqlite:///quiteria.sqlite'
+    DB_PATH = DB_FILE
+
+
+    @staticmethod
+    def create_sessions_table(engine):
+        metadata = MetaData()
+        Table(
+            SQLiteDB.SESSIONS_TABLE, metadata,
+            Column(SQLiteDB.C_SESSION_ID, Integer,
+                   primary_key=True,
+                   autoincrement=True,
+                   nullable=False),
+            Column(SQLiteDB.C_SESSION_USER_ID, Integer,
+                   nullable=False,
+                   unique=True),
+            Column(SQLiteDB.C_SESSION_LAST_ACCESS, Date,
+                   nullable=False),
+            Column(SQLiteDB.C_SESSION_ISLOGGED, Boolean,
+                   nullable=False,
+                   unique=True)
+        ).create(engine, checkfirst=True)
+
 
     @staticmethod
     def create_user_table(engine):
@@ -47,6 +79,8 @@ class SQLiteDB:
             Column(SQLiteDB.C_USER_EMAIL, String(70),
                    nullable=False,
                    unique=True),
+            Column(SQLiteDB.C_USER_PWD, String(16),
+                   nullable=False),
             Column(SQLiteDB.C_USER_LAB, String(50)),
             Column(SQLiteDB.C_USER_ROLE, String(50)),
             Column(SQLiteDB.C_USER_ACTIVE, Boolean,
@@ -78,7 +112,8 @@ class SQLiteDB:
 
     @staticmethod
     def create_tables():
-        engine = create_engine(SQLiteDB.DB_FILE, echo=True)
+        engine = create_engine(SQLiteDB.DB_PATH, echo=True)
+        SQLiteDB.create_sessions_table(engine)
         SQLiteDB.create_user_table(engine)
         SQLiteDB.create_room_table(engine)
         SQLiteDB.create_maintenance_table(engine)
@@ -100,7 +135,7 @@ class SQLiteDB:
 
     @staticmethod
     def drop_tables():
-        engine = create_engine(SQLiteDB.DB_FILE, echo=True)
+        engine = create_engine(SQLiteDB.DB_PATH, echo=True)
         SQLiteDB.drop_user_table(engine)
         SQLiteDB.drop_room_table(engine)
         SQLiteDB.drop_maintenance_table(engine)
@@ -113,7 +148,7 @@ class UserDAO:
 
     def __init__(self):
         self.metadata = MetaData()
-        self.engine = create_engine(SQLiteDB.DB_FILE, echo=True)
+        self.engine = create_engine(SQLiteDB.DB_PATH, echo=True)
 
     def insert_user(self, entries):
         values = []
@@ -152,13 +187,14 @@ class UserDAO:
 
     @staticmethod
     def create_user(data):
-        user = User()
-
-        if data is None:
-            raise RuntimeError("Dados invalidos")
-        else:
-            user.from_dict(data)
-        return user
+        # TODO :
+        # user = User()
+        # if data is None:
+        #     raise RuntimeError("Dados invalidos")
+        # else:
+        #     user.from_dict(data)
+        # return user
+        pass
 
 
 class RoomDAO:
