d9c9ddd072c232ab1dde419d03124697cf8d8bbe
aldemarf <aldemar.poli@gmail.com>
2019-07-29 00:01:38 -0300

    Fixed methods + parameters renames

--- src/quiteria/persistence/sqlite.py
+++ src/quiteria/persistence/sqlite.py
@@ -3,7 +3,6 @@ from sqlalchemy import (MetaData, create_engine, Table,
 
 
 class SQLiteDB:
-
     # TABLES
     USER_TABLE = 'users'
     USER_SESSION_TABLE = 'users_sessions'
@@ -25,7 +24,7 @@ class SQLiteDB:
     C_USER_PWD = 'pwd'
     C_USER_LAB = 'lab'
     C_USER_ROLE = 'role'
-    C_USER_ACTIVE = 'isActive'
+    C_USER_ACTIVE = 'active'
 
     # ROOM COLUMNS
     C_ROOM_ID = 'id'
@@ -37,7 +36,6 @@ class SQLiteDB:
     DB_MEMORY = 'sqlite:///:memory:'
     DB_FILE = 'sqlite:///quiteria.sqlite'
     DB_PATH = DB_FILE
-
 
     @classmethod
     def create_sessions_table(cls, engine):
@@ -58,7 +56,6 @@ class SQLiteDB:
                    unique=True)
         ).create(engine, checkfirst=True)
 
-
     @staticmethod
     def create_user_table(engine):
         metadata = MetaData()
@@ -66,25 +63,22 @@ class SQLiteDB:
             SQLiteDB.USER_TABLE, metadata,
             Column(SQLiteDB.C_USER_ID, Integer,
                    primary_key=True,
-                   autoincrement=True,
-                   nullable=False),
+                   autoincrement=True),
             Column(SQLiteDB.C_USER_TG_ID, Integer,
                    nullable=False,
                    unique=True),
             Column(SQLiteDB.C_USER_NAME, String(150),
                    nullable=False),
             Column(SQLiteDB.C_USER_EMAIL, String(70),
-                   nullable=False,
+                   nullable=True,
                    unique=True),
             Column(SQLiteDB.C_USER_PWD, String(16),
                    nullable=False),
             Column(SQLiteDB.C_USER_LAB, String(50)),
             Column(SQLiteDB.C_USER_ROLE, String(50)),
             Column(SQLiteDB.C_USER_ACTIVE, Boolean,
-                   nullable=False,
-                   default=False)
+                   nullable=False)
         ).create(engine, checkfirst=True)
-
 
     @staticmethod
     def create_maintenance_table(engine):
@@ -97,7 +91,6 @@ class SQLiteDB:
                    nullable=False)
         ).create(engine, checkfirst=True)
 
-
     @staticmethod
     def create_room_table(engine):
         metadata = MetaData()
@@ -109,7 +102,6 @@ class SQLiteDB:
                    nullable=False)
         ).create(engine, checkfirst=True)
 
-
     @classmethod
     def create_tables(cls):
         engine = create_engine(cls.DB_PATH, echo=True)
@@ -117,7 +109,6 @@ class SQLiteDB:
         cls.create_user_table(engine)
         cls.create_room_table(engine)
         cls.create_maintenance_table(engine)
-
 
     @classmethod
     def drop_user_table(cls, engine):
@@ -141,6 +132,7 @@ class SQLiteDB:
         cls.drop_room_table(engine)
         cls.drop_maintenance_table(engine)
 
+
 #########################################################
 
 
@@ -150,26 +142,23 @@ class UserDAO:
         self.metadata = MetaData()
         self.engine = create_engine(SQLiteDB.DB_PATH, echo=True)
 
-    def insert_user(self, entries):
-        values = []
-        users = entries
-
-        if not isinstance(entries, list):
-            users = [entries]
-        else:
-            for item in users:
-                values.append(dict(
-                    name=item.name,
-                    email=item.email,
-                    lab=item.lab,
-                    role=item.role,
-                    active=item.active
-                ))
+    def insert_user(self, user):
+        values = dict(
+            telegram_id=user.telegramID,
+            name=user.name,
+            email=user.email,
+            pwd=user.password,
+            lab=user.lab,
+            role=user.role,
+            active=user.active)
 
         user_table = Table(SQLiteDB.USER_TABLE, self.metadata,
                            autoload=True,
                            autoload_with=self.engine)
-        result = user_table.insert().values(users)
+        stmt = user_table.insert().values(values)
+
+        with self.engine.connect() as connection:
+            result = connection.execute(stmt)
 
         return result.inserted_primary_key
 
@@ -177,10 +166,10 @@ class UserDAO:
         user = Table(SQLiteDB.USER_TABLE, self.metadata,
                      autoload=True,
                      autoload_with=self.engine)
+        sel = user.select()
+        sel = sel.where(user.c.telegram_id == telegram_id)
 
         with self.engine.connect() as connection:
-            sel = user.select()
-            sel = sel.where(user.c.telegram_id == telegram_id)
             result = connection.execute(sel).fetchall()
 
         return result
