87846876165093c2114091c9045eaaf93c37e2f0
aldemarf <aldemar.poli@gmail.com>
2019-07-30 09:25:26 -0300

    MQTT subscriber

--- src/quiteria/domain/mqtt_subscriber.py
+++ src/quiteria/domain/mqtt_subscriber.py
@@ -1,46 +1,12 @@
 import logging
-import paho.mqtt.client as mqtt
 import paho.mqtt.subscribe as subscribe
-import sys
-from var import *
+from quiteria.resources.var import *
 
 logging.getLogger().setLevel(logging.INFO)
 
-subscribeTopic = DHT_TOPIC
 
-def on_connect(client, userdata, flags, rc):
-    logging.info('Conectando ao broker... Status : {}'.format(rc))
+def last_messages(topics=('th0/temperature')):
 
-    client.subscribe(subscribeTopic)
-
-
-def on_message(client, userdata, msg):
-    MensagemRecebida = str(msg.payload)
-
-    logging.info('Tópico: {}    Mensagem: {}'
-                 .format(msg.topic, MensagemRecebida))
-
-def start_subscribe():
-    try:
-        logging.info('Inicializando MQTT...')
-
-        client = mqtt.Client()
-        client.on_connect = on_connect
-        client.on_message = on_message
-
-        client.connect(BROKER_URL, BROKER_PORT, KEEP_ALIVE_BROKER)
-        client.loop_forever()
-
-    except KeyboardInterrupt:
-        logging.error('Saida forçada. Encerrando aplicação...')
-        sys.exit(0)
-
-
-def last_Message(topics=(TEMPERATURE_TOPIC, HUMIDITY_TOPIC)):
     message = subscribe.simple(topics, hostname=BROKER_URL,
-                                retained=False,
-                                msg_count=1)
-    return message
-
-if __name__ == '__main__':
-    last_Message()
+                               retained=True, msg_count=1)
+    return message.payload
